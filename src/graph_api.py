80c5c574850f5ba1d0b7a03e26a632af5234f7a0
Payam Mokhtarian <131449627+payam-mokhtarian@users.noreply.github.com>
2023-04-27 01:07:56 +1000

    Update graph_api.py

--- src/graph_api.py
+++ src/graph_api.py
@@ -10,11 +10,11 @@ Original file is located at
 import requests
 import json
 
-HOST = 'http://3.236.93.178'
+HOST = ''
 PORT = '7474'
 ENDPOINT = '/db/neo4j/tx'
 USER = 'neo4j'
-PWD = 'growth-reservists-curves'
+PWD = ''
 
 url = HOST+':'+PORT+ENDPOINT
 print(url)
